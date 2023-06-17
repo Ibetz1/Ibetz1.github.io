15543ebb7c0c28d0606995d4a8d176f24d851fbd
1SliiX1 <63432408+1SliiX1@users.noreply.github.com>
2023-06-16 18:04:42 -0700

    subtitle

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./**/*.{html,js}"],
   theme: {
     extend: {
       keyframes: {
@@ -27,6 +27,8 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require("tailwindcss-animation-delay")
+  ],
 }
 
